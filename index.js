81980e5ba0d51db6f3ecc7171fce318e4e7f884b
Zrossiz <soon.pluxury@mail.ru>
2024-03-13 20:14:21 +0300

    feat: added question route

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 require('dotenv').config();
 const axios = require('axios');
+const cors = require('cors');
 const express = require('express');
 const { Telegraf } = require('telegraf');
 
 const app = express();
+const router = express.Router();
 const port = process.env.PORT;
 const chatId = process.env.CHAT_ID;
 const botId = process.env.BOT_ID;
 
 const bot = new Telegraf(botId);
+
+app.use(express.json());
+app.use(cors());
+app.use('/bot', router);
+
+router.post('/question', async (req, res) => {
+    try {
+        const { name, phone, question, link } = req.body;
+        const message = `<b>Новая заявка!</b>\n\n<b>Имя: </b>${name}\n<b>Телефон: </b>${phone}\n<b>Вопрос: </b>${question}`;
+        
+        const replyMarkup = {
+            inline_keyboard: [
+                [
+                    { text: 'Открыть страницу', url: link }
+                ]
+            ]
+        };
+
+        await bot.telegram.sendMessage(
+            chatId, 
+            message,
+            {
+                parse_mode: 'HTML',
+                reply_markup: replyMarkup
+            }
+        ).then(() => {
+            res.status(200).json({ message: 'Сообщение успешно отправлено' });
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).json({ message: 'Ошибка при отправке сообщения' });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Ошибка сервера' });
+    }
+});
 
 app.listen(port, (err) => {
     if (err) {

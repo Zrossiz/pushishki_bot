3e5b9c12c1a86c1e00bc19e4e488a958301f5b5e
Zrossiz <soon.pluxury@mail.ru>
2024-03-14 09:59:09 +0300

    feat: added review route

--- index.js
+++ index.js
@@ -71,6 +71,38 @@ router.post('/order', async (req, res) => {
     }
 })
 
+router.post('/review', async (req, res) => {
+    try {
+        const { id, username, rating, title, description } = req.body;
+
+        console.log(req.body);
+        const message = `<b>Новый отзыв!</b>\n\n<b>Имя: </b>${username}\n<b>Рейтинг: </b>${rating}\n<b>Заголовок: </b>${title}\n<b>Комментарий: </b>${description}`;
+        const replyMarkup = {
+            inline_keyboard: [
+                [
+                    { text: 'Активировать', callback_data: `activate_${id}` }
+                ]
+            ]
+        };
+        await bot.telegram.sendMessage(
+            chatId, 
+            message,
+            {
+                parse_mode: 'HTML',
+                reply_markup: replyMarkup
+            }
+        ).then(() => {
+            res.status(200).json({ message: 'Сообщение успешно отправлено' });
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).json({ message: 'Ошибка при отправке сообщения' });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Ошибка сервера' });   
+    }
+});
+
 app.listen(port, (err) => {
     if (err) {
         console.log(err);
@@ -79,16 +111,13 @@ app.listen(port, (err) => {
     console.log(`Bot started on port: ${port}`);
 });
 
+bot.on('callback_query', async (ctx) => {
+    const callbackData = ctx.update.callback_query.data;
+    if (callbackData.startsWith('activate')) {
+        const id = callbackData.split('_')[1];
+        await axios.put(`${process.env.API_URL}/review/${id}`)
+        await ctx.reply(`Отзыв активирован ${id}`);
+    }
+});
+
 bot.launch();
-
-//   async sendReview(chatId: number, reviewDto: ReviewDto) {
-//     try {
-//       const message: string = `<b>Новый отзыв!</b>\n\n<b>Имя: </b>${reviewDto.username}\n<b>Рейтинг: </b>${reviewDto.rating}\n<b>Заголовок: </b>${reviewDto.title}\n<b>Комментарий: </b>${reviewDto.description}`;
-//       await this.telegram.telegram.sendMessage(chatId, message, { parse_mode: 'HTML' });
-//     } catch (err) {
-//       console.log(err);
-//       throw new InternalServerErrorException('Ошибка сервера');
-//     }
-//   }
-
-// }
